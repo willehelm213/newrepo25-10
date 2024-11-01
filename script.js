d87e57ead02e553d3808752dd68368ad6ad6b680
Guilherme Simões <160043524+willehelm213@users.noreply.github.com>
2024-11-01 11:01:00 -0400

    Add files via upload
    01/11

---
+++ script.js
@@ -0,0 +1,33 @@
+const transactionUl = document.querySelector("#transactions");
+//console.log(transactionUl);
+
+const dummyTransactions = [
+  { id: 1, name: "Bolo de brigadeiro", amount: -20 },
+  { id: 2, name: "Salário", amount: 300 },
+  { id: 3, name: "Torta de frango", amount: -10 },
+  { id: 4, name: "Violão", amount: 150 },
+];
+
+const addTransactionIntoDOM = (transaction) => {
+  const operator = transaction.amount < 0 ? "-" : "+";
+  const CSSClass = transaction.amount < 0 ? "minus" : "plus";
+  const amoutWithoutOperator = Math.abs(transaction.amount);
+  const li = document.createElement("li");
+
+  li.classList.add(CSSClass);
+  
+  li.innerHTML = `
+        ${transaction.name} <span> ${operator} R$ ${amoutWithoutOperator}</span>`;
+  transactionUl.append(li);
+
+  console.log(li);
+  //console.log(operator);
+
+  {
+    // <li class="minus">
+    //     Salário <span>-$400</span><button class="delete-btn">x</button>
+    // </li>
+  }
+};
+
+addTransactionIntoDOM(dummyTransactions[1]);
